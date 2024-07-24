c9ad28373c3119292d9f252d527ea5ca20b769d9
AbinShinu <ams272826@gmail.com>
2024-07-24 21:46:57 +0530

    just registration form

--- registratin.php
+++ registratin.php
@@ -5,6 +5,35 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+<h2>Fill The Form Correctly</h2>
+<table>
+<div class="div">
+    <form action="" method="POST">
+        <div>
+        <tr><td><label for="fname">FIRST NAME</label></td>
+        <td> <input type="text" name="fname" id="fname"></td></tr>
+        </div>
+        <div class="inputlayer">
+        <tr><td><label for="lname">LAST NAME</label></td>
+        <td>  <input type="text" name="lname" id="lname"></td></tr>
+        </div>
+        <div class="inputlayer">
+        <tr><td> <label for="mail">EMAIL</label></td>
+        <td> <input type="email" name="mail" id="mail"></td></tr>      
+        </div>
+        <div class="inputlayer">
+        <tr><td> <label for="phonenum">MOBILE NUMBER</label></td>
+        <td> <input type="integer" name="phonenum" id="pnum"></td></tr>           
+        </div>
+        <tr> </tr>
+        <div class="inputlayer">
+        <tr><td></td><td>  <input type="submit" value="submit"></td></tr>
+        </div>
+
+    </form>
+</div>
+</table>
+    </div>
 </body>
 </html>
