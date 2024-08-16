050c130b904e6a45ea5c980721f99e01277d9928
AjaykiranPT <ajaykiran1221@gmail.com>
2024-08-16 13:12:11 +0530

    forgot

--- forgotValidation.php
+++ forgotValidation.php
@@ -28,41 +28,34 @@
         if ($stmt->fetch()) {
             
             elseif($stored_type === 'student'){
-                $stmt = $conn->prepare("student_detatils WHERE account_email = ? AND PhoneNum=?");
+                $stmt = $conn->prepare("SELECT PhoneNum FROM student_detatils WHERE account_email = ?");
                 if ($stmt === false) {
                     die("Prepare failed: " . $conn->error);
                 }
-                $stmt->bind_param('ss', $forgot_email, $forgot_phone);
+                $stmt->bind_param('s', $forgot_email);
                 $stmt->execute();
-                $stmt->bind_result($stored_type);
+                $stmt->bind_result($stored_phone);
             }
             elseif($stored_type === 'company'){
-                $stmt = $conn->prepare("SELECT account_type FROM account_login WHERE account_email = ?");
+                $stmt = $conn->prepare("SELECT PhoneNum FROM company_detatils WHERE account_email = ?");
                 if ($stmt === false) {
-                die("Prepare failed: " . $conn->error);
+                    die("Prepare failed: " . $conn->error);
                 }
                 $stmt->bind_param('s', $forgot_email);
                 $stmt->execute();
-                $stmt->bind_result($stored_type);
+                $stmt->bind_result($stored_phone);
             }
-            else{
-                $error_message = urldecode('User not found');
-                header("Location: login.php?error=$error_message");
-                exit();
-            }
-            if (password_verify($input_password, $stored_password)) {
-                
-            }   
-            else {
-                $error_message = urldecode('Incorrect password. Please try again.');
-                header("Location: login.php?error=$error_message");
-                exit();    
+            if($stored_phone.equals($forgot_phone)){
+                $stmt = $conn->prepare("UPDATE account_login SET account_password= ? WHERE account_email = ?");
+                if ($stmt === false) {
+                    die("Prepare failed: " . $conn->error);
+                }
+                $stmt->bind_param('ss', $forgot_password, $forgot_email);
+                $stmt->execute();   
             }
         }
         else {
-            $error_message = urldecode('User not found');
-            header("Location: login.php?error=$error_message");
-            exit();
+            echo "<script>alert('User not found');</script>;";
         }
 
 
