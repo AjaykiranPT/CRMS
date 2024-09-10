6709b663d628c1b930e6ddb5b1d533235054c8cc
AjaykiranPT <ajaykiran1221@gmail.com>
2024-09-10 09:09:57 +0530

    DB Added

--- DB.sql
+++ DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2024 at 05:43 PM
+-- Generation Time: Sep 10, 2024 at 05:38 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
